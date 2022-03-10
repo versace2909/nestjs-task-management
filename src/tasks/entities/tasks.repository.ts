b34c7eb082c05e4ef47d402f2b372217c40f995f
Dung Viet <vietdung299@gmail.com>
2022-03-10 21:40:03 +0700

    update code

---
+++ src/tasks/entities/tasks.repository.ts
@@ -0,0 +1,38 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from '../createTaskDto';
+import { GetTaskFilterDto } from '../get-task-filter.dto';
+import { TaskStatus } from '../task-status.enum';
+import { Task } from './task.entity';
+
+@EntityRepository(Task)
+export class TasksRepository extends Repository<Task> {
+  async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
+    const query = this.createQueryBuilder('task');
+
+    if (filterDto.status) {
+      query.andWhere('task.status = :status', { status: filterDto.status });
+    }
+
+    if (filterDto.search) {
+      query.andWhere(
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        { search: `%${filterDto.search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+    const task = this.create({
+      title: title,
+      description: description,
+      status: TaskStatus.OPEN,
+    });
+
+    await this.save(task);
+    return task;
+  }
+}
---
+++ src/tasks/get-task-filter.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { TaskStatus } from './task-status.enum';
+
+export class GetTaskFilterDto {
+  @IsEnum(TaskStatus)
+  @IsOptional()
+  @ApiPropertyOptional()
+  status: TaskStatus;
+
+  @IsString()
+  @ApiPropertyOptional()
+  @IsOptional()
+  search: TaskStatus;
+}
